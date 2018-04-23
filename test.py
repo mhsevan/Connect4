97c92511224ec7ea62a88f612c4dd33514b97c4b
mhsevan <me.mhsevan@gmail.com>
2018-04-23 19:12:14 -0400

    cleanup files

--- test.py
+++
@@ -1,7 +0,0 @@
-import json
-
-json_str = '{"username": "u1", "response_type": "join", "response_data": "", "response_to_type": "", "response_to_data": ""}'
-json_data = json.loads(json_str)
-
-print(json_data)
-print(json_data['username'])
